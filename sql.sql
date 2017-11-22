5d4c8941e4fdeb1275eb2862f2a2d30ac58e2757
AndreVictor60 <andre.victor60@free.fr>
2017-11-22 08:43:42 +0100

    fini

---
+++ sql.sql
@@ -0,0 +1,12 @@
+Exercice 1:
+
+delete from languages where language='HTML';
+
+Exercice 2:
+
+update frameworks set framework='Symfony2' where framework='Symfony';
+
+Exercice 3:
+
+update languages set version='version 5.1' where id='1';
+
